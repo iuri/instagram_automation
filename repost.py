dec17898947fbe17c2fd0860f673cb4c5da316b3
Iuri Sampaio <iuri.sampaio@bairesdev.com>
2025-05-04 15:15:24 -0300

    fixed loop searching for stories

--- repost.py
+++ repost.py
@@ -1,10 +1,7 @@
 import os
 from instagrapi import Client
 from dotenv import load_dotenv
-
 import random
-import requests
-
 
 load_dotenv()
 ACCOUNT_USERNAME = os.environ.get("USERNAME")
@@ -13,7 +10,7 @@ ACCOUNT_PASSWORD = os.environ.get("PASSWORD")
 def authenticate():
     cl = Client()
     cl.login(ACCOUNT_USERNAME, ACCOUNT_PASSWORD)
-    print(f"Authenticated as {ACCOUNT_USERNAME}")
+    # print(f"Authenticated as {ACCOUNT_USERNAME}")
     return cl
 
 def get_following_user_ids(cl):
@@ -27,7 +24,7 @@ def get_following_user_ids(cl):
 
 
 
-def get_following_usernames(cl, exclude_list=None):
+def get_following_usernames(cl):
     """
     Get the list of usernames the account is following.
     """
@@ -35,15 +32,9 @@ def get_following_usernames(cl, exclude_list=None):
         user_id = cl.user_id_from_username(ACCOUNT_USERNAME)
         user_ids = cl.user_following(user_id)
         user_names = [user.username for user in user_ids.values()]
-
-        # Filtrer les noms d'utilisateur pour exclure ceux dans la liste d'exclusion
-        user_names = [username for username in user_names if username not in exclude_list]
-
-
-
-        print(f"Found {len(user_names)} followed accounts.")
+        # print(f"Found {len(user_names)} followed accounts.")
     except Exception as e:
-        print(f"Error retrieving followed accounts: {e}")
+        # print(f"Error retrieving followed accounts: {e}")
         return []
     return user_names
 
@@ -51,7 +42,7 @@ def get_recent_posts_and_stories(cl, user_ids, posts_per_user=3):
     """
     Retrieve recent posts and stories from a list of user IDs.
     """
-    print(f"Fetching recent posts and stories for {len(user_ids)} users...")
+    # print(f"Fetching recent posts and stories for {len(user_ids)} users...")
     data = {}
     for uid in user_ids:
         username = cl.user_info(uid).username
@@ -73,10 +64,10 @@ def get_stories_from_user(cl, username):
     try:
         user_id = cl.user_id_from_username(username)
         stories = cl.user_stories(user_id)
-        print(f"Found {len(stories)} stories for user '{username}'.")
+        # print(f"Found {len(stories)} stories for user '{username}'.")
         return stories
     except Exception as e:
-        print(f"Error retrieving stories for {username}: {e}")
+        # print(f"Error retrieving stories for {username}: {e}")
         return []
 
 
@@ -85,107 +76,88 @@ def repost_story(cl, story):
     Download a story and repost it to your own story.
     """
     try:
-        user_info = cl.user_info(story.user.pk)
-        if user_info.account_type in [2, 3]:  # 2: Business, 3: Creator
-            # Download the story media
-            media_path = cl.story_download(story.pk)
-            print(f"Downloaded story to {media_path}")
+        # Download the story media
+        media_path = cl.story_download(story.pk)
+        # print(f"Downloaded story to {media_path}")
 
-            # Safely get caption if it exists, else use empty string
-            caption = getattr(story, "caption", "")
+        # Safely get caption if it exists, else use empty string
+        caption = getattr(story, "caption", "")
 
-            # Ajouter des hashtags à la légende
-            # hashtags = "#inspiration #photography #AIgenerated #iurixtech #artificialintelligence #techinnovation, #digitalart, #creativeAI"
-            hashtags = "#iurixtech"
-            caption = f"{caption}\n\n{hashtags} owner: @{story.user.username}"
+        # Ajouter des hashtags à la légende
+        # hashtags = "#inspiration #photography #AIgenerated #iurixtech #artificialintelligence #techinnovation, #digitalart, #creativeAI"
+        hashtags = "#iurixtech"
+        caption = f"{caption}\n\n{hashtags} owner: @{story.user.username}"
 
-            # Ajouter une localisation
-            locations = []
-            if hasattr(story, "locations") and story.locations:
-                location = story.locations[0]
-                print(f"Using location: {location.name}")
+        # Ajouter une localisation
+        locations = []
+        if hasattr(story, "locations") and story.locations:
+            locations = story.locations[0]
+            # print(f"Using location: {location.name}")
 
-            # Ajouter des stickers si disponibles
-            stickers = []
-            if hasattr(story, "stickers") and story.stickers:
-                stickers = story.stickers
-                print(f"Found {len(stickers)} stickers in the original story.")
+        # Ajouter des stickers si disponibles
+        stickers = []
+        if hasattr(story, "stickers") and story.stickers:
+            stickers = story.stickers
+            # print(f"Found {len(stickers)} stickers in the original story.")
 
-
-            # Déterminer le type de média et republier en conséquence
-            if story.media_type == 1:  # Photo
-                cl.photo_upload_to_story(media_path, caption=caption, locations=locations, stickers=stickers)
-                print("Reposted photo story with hashtags and location.")
-            elif story.media_type == 2:  # Vidéo
-                cl.video_upload_to_story(media_path, caption=caption, locations=locations, stickers=stickers)
-                print("Reposted video story with hashtags and location.")
-            else:
-                print("Unsupported story media type.")
+        # Déterminer le type de média et republier en conséquence
+        if story.media_type == 1:  # Photo
+            cl.photo_upload_to_story(media_path, caption=caption, locations=locations, stickers=stickers)
+            print("Reposted photo story with hashtags and location.")
+        elif story.media_type == 2:  # Vidéo
+            cl.video_upload_to_story(media_path, caption=caption, locations=locations, stickers=stickers)
+            # print("Reposted video story with hashtags and location.")
         else:
-            print(f"User {story.user.username} is not a business or creator account. Skipping story repost.")
+            # print("Unsupported story media type.")
+            return "Unsupported story media type."
     except Exception as e:
-        print(f"Error reposting story: {e}")    
+        # print(f"Error reposting story: {e}")
+        return f"Error reposting story: {e}"
     return
-
-def download_image_from_url(url, output_path):
-    """
-    Télécharge une image à partir d'une URL et la sauvegarde dans un fichier local.
-
-    :param url: L'URL de l'image à télécharger.
-    :param output_path: Le chemin où l'image sera sauvegardée.
-    """
-    try:
-        response = requests.get(url, stream=True)
-        response.raise_for_status()  # Vérifie si la requête a réussi
-
-        with open(output_path, 'wb') as file:
-            for chunk in response.iter_content(1024):
-                file.write(chunk)
-
-        print(f"Image téléchargée et sauvegardée dans : {output_path}")
-    except Exception as e:
-        print(f"Erreur lors du téléchargement de l'image : {e}")
-
-
 
 
 def main():
     cl = authenticate()
-    user_ids = get_following_user_ids(cl)
-    print(f"Found {len(user_ids)} followed accounts.")
-    
-    exclude_list = ["hlacara", "lucastorreslta", "tomrlon", "mcjneto"]
+    # user_ids = get_following_user_ids(cl)
+    # print(f"Found {len(user_ids)} followed accounts.")
+    # exclude_list = ["hlacara", "lucastorreslta", "tomrlon", "mcjneto"]
     
     while True:
-        usernames = get_following_usernames(cl, exclude_list)
+        usernames = get_following_usernames(cl)
         # print(f"Found {usernames} followed accounts.")
         
         # select a random integer from 1 to 5 
         random_number = random.randint(1, 5)
-        print(f"Random number of users to select: {random_number}")
+        # print(f"Random number of users to select: {random_number}")
         
         # Select 3 random users from list
         random_users = random.sample(usernames, random_number)
         print(f"Selected random users: {random_users}")
-
         stories = []
         for username in random_users:
-            user_stories = get_stories_from_user(cl, username)
-            print(f"Stories objects: {len(user_stories)}")
-            # Check if there are any stories
-            if user_stories:
-                # print(f"Story: {user_stories[0]}")
-                stories.append(user_stories[0])
-            else:
-                print("No stories found to repost.")
+            user_info = cl.user_info_by_username(username)
+            # print(f"User info: {user_info}")
+            # user_info = cl.user_info(story.user.pk)
+            if user_info.account_type in [2, 3]:  # 2: Business, 3: Creator
+                user_stories = get_stories_from_user(cl, username)
+                # print(f"Stories objects: {len(user_stories)}")
+                # Check if there are any stories
+                if user_stories:
+                    # print(f"Story: {user_stories[0]}")
+                    stories.append(user_stories[0])
+                # else:
+                    # print("No stories found to repost.")
+            # else:
+                # print(f"User {story.user.username} is not a business or creator account. Skipping story repost.")
+                
 
         # Check if there are any stories to repost
         if stories:
             break
-    
+    # print(f"Found {len(stories)} stories to repost.")
     for story in stories:
         # Repost the story    
-        print(f"Reposting story...: {story}")
+        # print(f"Reposting story...: {story}")
         repost_story(cl, story)
 
 
